9d803a6cb5c432f66d2bee1dc88d101ead7fbbbf
javierruanohdez <javierruanohdez@gmail.com>
2026-03-19 23:16:42 +0000

    feat: scale crawler to nationwide airports with rate-limiting

--- flight-status-feeder/src/main/java/es/ulpgc/dacd/skydelay/flights/control/FlighteraCrawler.java
+++ flight-status-feeder/src/main/java/es/ulpgc/dacd/skydelay/flights/control/FlighteraCrawler.java
@@ -5,13 +5,37 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 public class FlighteraCrawler {
+    private static final List<String> ICAO_SPAIN_AIRPORTS = Arrays.asList(
+            "GCRR", "GCLP", "GCFV", "GCXO", "GCTS", "GCLA", "GCHI", "GCGM",
+            "LEMD", "LEBL", "LEPA", "LEMG", "LEAL", "LEVC", "LEZL", "LEIB", "LEBB", "LEST", "LEMH",
+            "LEAS", "LEXJ", "LEGR", "LEVT", "LEPP", "LELN", "LESA", "LEVD", "LELO", "LEHC",
+            "LEJR", "LEAM", "LERS", "LEGE", "LEBZ", "LECH", "GEML"
+    );
+
     public static void main(String[] args) {
-        String url = "https://www.flightera.net/en/airport/Gran+Canaria/GCLP/departure";
+        for (String code : ICAO_SPAIN_AIRPORTS) {
+            String airportUrl = "https://www.flightera.net/en/airport/_/" + code + "/departure";
+            System.out.println("\n>>> Checking Airport: " + code);
+            crawlAirport(airportUrl);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void crawlAirport(String airportUrl){
         try {
-            Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36").get();
+            Document doc = Jsoup.connect(airportUrl).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+                    .timeout(15000)
+                    .get();
             Elements rows = doc.select("table tbody tr");
-            System.out.println("--- NATIONAL FLIGHTS FOUND ---");
             for (Element row : rows) {
                 Element spainFlag = row.selectFirst("img[alt*='Spain']");
                 if (spainFlag != null) {
@@ -29,4 +53,3 @@ public class FlighteraCrawler {
         }
     }
 }
-
