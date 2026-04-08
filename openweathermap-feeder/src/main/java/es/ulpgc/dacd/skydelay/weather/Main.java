97e1b3563c5298e437c9a85d6fb0c0007c2ae97f
Lucasmendo30 <lucasmendozaguancha12@gmail.com>
2026-04-08 16:27:25 +0100

    feat: delete scheduler and simplify control class

--- openweathermap-feeder/src/main/java/es/ulpgc/dacd/skydelay/weather/Main.java
+++ openweathermap-feeder/src/main/java/es/ulpgc/dacd/skydelay/weather/Main.java
@@ -1,6 +1,7 @@
 package es.ulpgc.dacd.skydelay.weather;
 
 import es.ulpgc.dacd.skydelay.weather.control.*;
+import es.ulpgc.dacd.skydelay.weather.model.Airport;
 import es.ulpgc.dacd.skydelay.weather.model.Weather;
 
 import java.util.List;
@@ -10,73 +11,14 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         AirportsReader reader = new AirportsReader();
+        List<Airport> airports = reader.read();
         WeatherParser parser = new WeatherParser();
-        WeatherFeeder feeder = new OpenWeatherMapFeeder(reader, parser);
+        WeatherFeeder feeder = new OpenWeatherMapFeeder(parser);
         WeatherStore store = new SqliteWeatherStore("storage/db/weather.db");
-        Control control = new Control(feeder, store);
-        WeatherScheduler scheduler = new WeatherScheduler(control);
+        Control control = new Control(feeder, store, airports);
 
-        Scanner scanner = new Scanner(System.in);
-        boolean running = true;
-
-        System.out.println("OpenWeatherMap feeder CLI");
-
-        while (running) {
-            System.out.println("\nSeleccione una opción:");
-            System.out.println("1. Obtener datos meteorológicos actuales de todos los aeropuertos españoles");
-            System.out.println("2. Almacenar datos en la base de datos");
-            System.out.println("3. Consultar información meteorológica de un aeropuerto específico");
-            System.out.println("4. Iniciar captura de datos periódica cada 6 horas");
-            System.out.println("5. Detener captura de datos periódica");
-            System.out.println("0. Salir");
-            System.out.print("> ");
-
-            String option = scanner.nextLine();
-
-            switch (option) {
-                case "1":
-                    System.out.println("Consultando API...");
-                    feeder.fetch().forEach(w -> System.out.println(w.toString()));
-                    break;
-
-                case "2":
-                    System.out.println("Iniciando proceso de persistencia...");
-                    control.execute();
-                    break;
-
-                case "3":
-                    System.out.print("Introduzca el código ICAO del aeropuerto (ej. GCLP): ");
-                    String icaoInput = scanner.nextLine().toUpperCase().trim();
-                    searchForIcao(feeder, icaoInput);
-                    break;
-
-                case "4":
-                    scheduler.start(6, TimeUnit.HOURS);
-                    break;
-                case "5":
-                    scheduler.stop();
-                    break;
-
-                case "0":
-                    running = false;
-                    System.out.println("Cerrando aplicación.");
-                    break;
-
-                default:
-                    System.out.println("Opción no válida.");
-            }
-        }
-        scanner.close();
+        System.out.println("Iniciando proceso de persistencia...");
+        control.execute();
     }
 
-    private static void searchForIcao(WeatherFeeder feeder, String icao) {
-        List<Weather> weathers = feeder.fetch();
-        weathers.stream()
-                .filter(w -> w.icao().equalsIgnoreCase(icao))
-                .findFirst()
-                .ifPresentOrElse(
-                        w -> System.out.println("Datos encontrados: " + w),
-                        () -> System.out.println("No se encontraron datos para el código: " + icao)
-                );
-    }
 }
--- openweathermap-feeder/src/main/java/es/ulpgc/dacd/skydelay/weather/control/WeatherFeeder.java
+++ openweathermap-feeder/src/main/java/es/ulpgc/dacd/skydelay/weather/control/WeatherFeeder.java
@@ -1,9 +1,10 @@
 package es.ulpgc.dacd.skydelay.weather.control;
 
+import es.ulpgc.dacd.skydelay.weather.model.Airport;
 import es.ulpgc.dacd.skydelay.weather.model.Weather;
 
 import java.util.List;
 
 public interface WeatherFeeder {
-    List<Weather> fetch();
+    List<Weather> fetch(Airport airport);
 }
