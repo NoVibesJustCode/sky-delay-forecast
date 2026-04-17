8145c549e746343b7599f39344f2855c93fa2c68
javierruanohdez <javierruanohdez@gmail.com>
2026-04-17 11:35:45 +0100

    fix: prevent link data loss using date-based file rotation

--- flight-status-feeder/src/main/java/es/ulpgc/dacd/skydelay/flights/Main.java
+++ flight-status-feeder/src/main/java/es/ulpgc/dacd/skydelay/flights/Main.java
@@ -3,6 +3,8 @@ package es.ulpgc.dacd.skydelay.flights;
 import es.ulpgc.dacd.skydelay.flights.control.*;
 import jakarta.jms.JMSException;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) throws JMSException {
         FlightCrawler crawler = new FlighteraCrawler();
@@ -14,7 +16,7 @@ public class Main {
             store.close();
         }));
 
-        Controller controller = new Controller(crawler, scraper, store, args[2]);
+        Controller controller = new Controller(crawler, scraper, store, args[2] + LocalDate.now().toString() + ".txt");
         controller.execute();
     }
 }
