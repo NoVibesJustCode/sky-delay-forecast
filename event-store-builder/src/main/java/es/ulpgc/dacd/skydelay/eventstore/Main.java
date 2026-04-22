8f407d867e9d0b1b38821d5a7205399405801cf2
javierruanohdez <javierruanohdez@gmail.com>
2026-04-22 10:18:42 +0100

    feat: implement Main class to orchestrate Event Store Builder

--- event-store-builder/src/main/java/es/ulpgc/dacd/skydelay/eventstore/Main.java
+++ event-store-builder/src/main/java/es/ulpgc/dacd/skydelay/eventstore/Main.java
@@ -1,4 +1,18 @@
 package es.ulpgc.dacd.skydelay.eventstore;
 
+import es.ulpgc.dacd.skydelay.eventstore.control.ActiveMQEventSubscriber;
+import es.ulpgc.dacd.skydelay.eventstore.control.FileEventStore;
+
 public class Main {
+    public static void main(String[] args) {
+        String brokerUrl = args[0];
+        FileEventStore store = new FileEventStore("eventstore");
+
+        ActiveMQEventSubscriber subscriber = new ActiveMQEventSubscriber(brokerUrl, "EventStoreBuilder", store);
+
+        subscriber.subscribe("weather");
+        subscriber.subscribe("flight");
+
+        System.out.println("Event Store Builder is running. Press Ctrl+C to stop.");
+    }
 }
