45a9f3cba444b87efd2094ed42c763ef6031460f
javierruanohdez <javierruanohdez@gmail.com>
2026-05-05 16:34:56 +0100

    fix: align EventStoreReader paths with directory hierarchy

--- business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/Main.java
+++ business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/Main.java
@@ -4,16 +4,17 @@ import es.ulpgc.dacd.skydelay.business.control.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.err.println("Error: Missing Broker URL. Usage: java -jar ... <broker_url>");
+        if (args.length < 4) {
+            System.err.println("Error: Missing arguments!");
             System.exit(1);
         }
 
         String brokerUrl = args[0];
         String dbPath = args[1];
         String csvPath = args[2];
+        String eventsPath = args[3];
 
-        Controller controller = new Controller(brokerUrl, dbPath, csvPath);
+        Controller controller = new Controller(brokerUrl, dbPath, csvPath, eventsPath);
         controller.execute();
     }
 }
