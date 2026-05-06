fec985c05185a8308d714974c55b5327a0572dea
javierruanohdez <javierruanohdez@gmail.com>
2026-05-06 16:53:51 +0100

    feat: implement PredictorService for automated classification

--- business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/control/DatamartManager.java
+++ business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/control/DatamartManager.java
@@ -179,6 +179,4 @@ public class DatamartManager {
         if (mins <= 60) return "moderate";
         return "severe";
     }
-
-
 }
---
+++ business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/control/PredictorService.java
@@ -0,0 +1,27 @@
+package es.ulpgc.dacd.skydelay.business.control;
+
+import es.ulpgc.dacd.skydelay.flights.model.Flight;
+import es.ulpgc.dacd.skydelay.weather.model.Weather;
+
+public class PredictorService {
+    private final DatamartManager datamart;
+    private KNNClassifier knn;
+
+    public PredictorService(DatamartManager datamart) {
+        this.datamart = datamart;
+        refreshModel();
+    }
+
+    public void refreshModel() {
+        this.knn = new KNNClassifier(datamart.loadTrainingData(), 5);
+    }
+
+    public void processNewFlight(Flight f) {
+        Weather forecast = datamart.fetchClosestWeather(f.origin(), f.departureTimeUTC());
+
+        if (forecast != null) {
+            String category = knn.predict(forecast.temp(), forecast.windSpeed(), forecast.visibility());
+            datamart.saveReadyToEatPrediction(f, category);
+        }
+    }
+}
