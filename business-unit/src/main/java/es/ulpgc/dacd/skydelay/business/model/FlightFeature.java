04c0c7f7ed42377ba358cd1629c28337d796d026
javierruanohdez <javierruanohdez@gmail.com>
2026-05-09 19:01:24 +0100

    feat: merge view layer for map and dashboard visualization

--- business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/model/FlightFeature.java
+++ business-unit/src/main/java/es/ulpgc/dacd/skydelay/business/model/FlightFeature.java
@@ -1,28 +1,10 @@
 package es.ulpgc.dacd.skydelay.business.model;
 
 public record FlightFeature(
-        String flightId,
-        String originIcao,
-        String destIcao,
-
-        double depTemp,
-        double depFeelsLike,
-        int depHumidity,
-        int depVisibility,
-        double depWindSpeed,
-        double depWindGust,
-        int depClouds,
-
-        double arrTemp,
-        double arrFeelsLike,
-        int arrHumidity,
-        int arrVisibility,
-        double arrWindSpeed,
-        double arrWindGust,
-        int arrClouds,
-
-        int distanceKm,
-        int departureDelay,
-        int arrivalDelay,
-        String delayCategory
-) {}
+        double temp,
+        double wind,
+        double gust,
+        double vis,
+        int distance,
+        String category)
+{}
