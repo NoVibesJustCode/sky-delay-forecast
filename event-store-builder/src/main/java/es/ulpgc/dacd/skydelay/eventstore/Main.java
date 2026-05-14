78ac14472795e2e5f569c9193145a47d1db144f2
javierruanohdez <javierruanohdez@gmail.com>
2026-05-14 23:37:26 +0100

    refactor: use program arguments for all modules

--- event-store-builder/src/main/java/es/ulpgc/dacd/skydelay/eventstore/Main.java
+++ event-store-builder/src/main/java/es/ulpgc/dacd/skydelay/eventstore/Main.java
@@ -4,13 +4,13 @@ import es.ulpgc.dacd.skydelay.eventstore.control.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 1) {
+        if (args.length < 2) {
             System.err.println("Error: Missing Broker URL. Usage: java -jar ... <broker_url>");
             System.exit(1);
         }
 
         String brokerUrl = args[0];
-        Controller controller = new Controller(brokerUrl, "eventstore");
+        Controller controller = new Controller(brokerUrl, args[1]);
         controller.execute();
     }
 }
